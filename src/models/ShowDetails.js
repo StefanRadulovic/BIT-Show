c585601966a3e3607ba5f76d83d6e5d9036d0756
Stefan Radulovic <stefan.radulovic@yahoo.com>
2018-08-10 13:12:51 +0200

    removed <>

--- src/models/ShowDetails.js
+++ src/models/ShowDetails.js
@@ -3,6 +3,9 @@ export default class ShowDetails {
         this.img = img;
         this.title = title;
         this.genres = genres;
-        this.summary = summary
+        this.summary=summary.replace(/(<([^>]+)>)/gi, '')
+        // let text = summary.split("");
+
+        // this.summary = text.filter(el => (el != '<' && el != '>' && el != '/')).join('')
     }
 }
